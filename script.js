d8259780e79db133ff2f0723060dfe9a9138e6ca
nadajackal <you@example.com>
2017-01-17 16:32:59 -0800

    added google map

---
+++ script.js
@@ -0,0 +1,22 @@
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelectorAll('#map').length > 0) {
+    if (document.querySelector('html').lang)
+      lang = document.querySelector('html').lang;
+    else
+      lang = 'en';
+
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyA7CyuuBY69Jl8MuVuh2JcpeSbpbcueaM4&language=' + lang;
+    document.getElementsByTagName('head')[0].appendChild(js_file);
+  }
+});
